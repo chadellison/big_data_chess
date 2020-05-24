8c4ee811fec685baeff6b30af82ae239381b1c68
Chad Ellison <chad.ellison0123@gmail.com>
2020-05-24 12:52:30 -0500

    add rails helper and spec helper

---
+++ app/models/abstraction.rb
@@ -0,0 +1,8 @@
+class Abstractions
+  def self.find_abstractions(position)
+    # look at current position --> return score for each next move
+    # look at stats on position
+    # look at related (child positions) and their stats (min max)
+    # pull abstractions out of position and run them through the nn
+  end
+end
