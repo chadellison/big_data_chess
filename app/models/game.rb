c22ee276ea47049b1a199a227fe9fdfecc1e61cb
Chad Ellison <chad.ellison0123@gmail.com>
2020-09-27 01:48:26 -0500

    update gems

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,18 +1,18 @@
 class Game < ApplicationRecord
-  # def play_self(fen_notation)
-  #   # move = make_random_move(fen_notation)
-  #   move = ChessValidator::MoveLogic.make_random_move(fen_notation)
-  #   if move.present?
-  #     # convert to new fen
-  #     play_self(fen_notation)
-  #   else
-  #     # return 1 if white wins
-  #     # return -1 if black wins
-  #     # 0
-  #     #
-  #     # result
-  #   end
-  # end
+  def play_self(fen_notation)
+    game_result = ChessValidator::GameLogic.find_game_result(fen_notation)
+
+    if game_result
+      puts 'the game is over...' + game_result
+      # update game object
+      # update positions
+    else
+      pieces_with_moves = ChessValidator::MoveLogic.find_next_moves(fen_notation)
+      new_fen_notation = ChessValidator::MoveLogic.make_random_move(fen_notation, pieces_with_moves)
+      puts new_fen_notation
+      play_self(new_fen_notation)
+    end
+  end
 
   def calculate_position_value(iterations, fen_notation)
     position_value = 0
